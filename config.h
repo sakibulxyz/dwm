6f5b546fc7a51704055ae61e18e189753e92b4f7
sakib <sakibul706@gmail.com>
2022-06-21 21:32:21 +0600

    libre office will open on 2nd desktop

--- config.h
+++ config.h
@@ -31,6 +31,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class                instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "libreoffice",		NULL,     NULL,           1 << 1,    0,          0,          -1,        -1 },
 	{ "firefox",   			NULL,     NULL,           1 << 2,    0,          0,          -1,        -1 },
 	{ "chromium",  			NULL,     NULL,           1 << 2,    0,          0,          -1,        -1 },
 	{ "brave",		   		NULL,     NULL,           1 << 2,    0,          0,          -1,        -1 },
